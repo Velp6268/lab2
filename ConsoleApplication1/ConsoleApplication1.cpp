a92e4391eb6d438603c872dca6a15032d7f44c1b
user <user@C1012>
2021-12-13 11:28:15 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int x = 2, y = 1;
+
+    if (x * y != 0 || y > x)
+    {
+ 
+        bool a = true, b = false;
+        bool res = (x * y != 0 || y > x);
+        cout << "Result: " << boolalpha << res;
+        return 0;
+
+
+    }
+
+    
+
+    system("pause");
+    return 0;
+}
